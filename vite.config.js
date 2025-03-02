5bf71e8f257c57c8b9ba492fd8eba7caed667f0f
Hrishikesh <143307598+Hrishikesh6187@users.noreply.github.com>
2025-03-01 22:09:48 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/PureSip/', // Base path for GitHub Pages deployment
+  base: '/trial/', // Base path for GitHub Pages deployment
 })
